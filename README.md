241ea3558126ab4af4f16e4d7576f84f54235a87
minataforest <minataforest@gmail.com>
2022-01-07 21:44:29 +0900

    220107 lv1 77484

---
+++ README.md
@@ -0,0 +1,7 @@
+Programmers coding test
+=======================
+
+https://programmers.co.kr/learn/challenges
+
+### 1. Lv1
+    2022-01-07 77484번 로또 최저 최고 순위
---
+++ lv1/220107_77484_lotto.py
@@ -0,0 +1,16 @@
+lottos=[0,0,0,0,0,0]
+win_nums=[38,19,20,40,15,25]
+
+# lottos=[45,4,35,20,3,9]
+# win_nums=[20,9,3,45,4,35]
+
+rank = {0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
+
+result = [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
+
+print(result)
+def solution(lottos, win_nums):
+
+    # set 교집합
+    rank = {0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
+    return rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]
